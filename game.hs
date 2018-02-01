14ef76e351a9d3ff58f895b5d673f74c317770df
Gabriel McDonald <gabrielmcdonald1@gmail.com>
2018-02-01 13:53:49 -0500

    deriving current board is now player's responsibility

--- game.hs
+++ game.hs
@@ -1,15 +1,9 @@
 module Game where
 
-import Board(startBoard,isMated,validMoves)
-import Square(Board,Color(White,Black),Move,move,targetPiece,toPos)
-import Player(Player(Player),PlayerType(Human),prompt,color)
+import Player(Player(Player),PlayerType(Human),prompt,hasLost)
+import Square(Color(White,Black),Move(Move))
 
 type Game = [Move]
-
-currentBoard :: Game -> Board
-currentBoard [] = startBoard
-currentBoard (m:ms) = (move (targetPiece m) (toPos m) lastBoard)
-    where lastBoard = currentBoard ms
 
 currentPlayer :: Game -> Color
 currentPlayer game = if even (length game) then White else Black
@@ -19,14 +13,12 @@ play :: Game
 play = play' []
 
 play' :: Game -> Game
-play' game = if isMated (currentPlayer game) (currentBoard game) then game else play' (nextTurn game)
+play' game = if hasLost (currentPlayer game) game then game else play' (nextTurn game)
 
 nextTurn :: Game -> Game
 nextTurn game = getMove game : game
 
 getMove :: Game -> Move
-getMove game = prompt player board moves
-    where moves = validMoves color board
-          board = currentBoard game
-          color = currentPlayer game
+getMove game = prompt player game
+    where color = currentPlayer game
           player = (Player Human color)
--- player.hs
+++ player.hs
@@ -1,13 +1,25 @@
 module Player where
 
-import Square(Board,Square,Color(White,Black),Pos,Move(Move))
-import Display(getMove)
+import Square(Board,Color,Move,move,targetPiece,toPos)
+import Board(startBoard,validMoves,isMated)
+import Display(Display,getMove)
 
 data Player = Player PlayerType Color
-data PlayerType = Human
+data PlayerType = Human | AI
+type Log = [Move]
 
 color :: Player -> Color
 color (Player _ c) = c
 
-prompt :: Player -> Board -> [Move] -> Move
-prompt (Player Human _) board ms = getMove (Display board) ms
+currentBoard :: Log -> Board
+currentBoard [] = startBoard
+currentBoard (m:ms) = (move (targetPiece m) (toPos m) lastBoard)
+    where lastBoard = currentBoard ms
+
+hasLost :: Color -> Log -> Bool
+hasLost col log = isMated col (currentBoard log)
+
+prompt :: Player -> Log -> Move
+prompt (Player Human col) log = getMove (Display board) ms
+    where board = currentBoard log
+          ms = validMoves col board

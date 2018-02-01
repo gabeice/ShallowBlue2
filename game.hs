373bcef6777f8fadd59a34ace97fa98fa15d9f10
Gabriel McDonald <gabrielmcdonald1@gmail.com>
2018-02-01 10:29:19 -0500

    derive current board from list of moves

--- game.hs
+++ game.hs
@@ -1,11 +1,18 @@
 module Game where
 
-import Board
-import Square(Square,Pos)
+import Board(startBoard)
+import Square(Board,Square,Pos,move)
 
 type Game = [Move]
 data Move = Move Square Pos
 
-currentBoard :: Game -> Board
+targetPiece :: Move -> Square
+targetPiece (Move p _) = p
 
-play :: Game
+toPos :: Move -> Pos
+toPos (Move _ p) = p
+
+currentBoard :: Game -> Board
+currentBoard [m] = (move (targetPiece m) (toPos m) startBoard)
+currentBoard (m:ms) = (move (targetPiece m) (toPos m) lastBoard)
+    where lastBoard = currentBoard ms

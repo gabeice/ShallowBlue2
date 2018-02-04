9350e7858d9915a2ea84b313af80eda43469b8f8
Gabriel McDonald <gabrielmcdonald1@gmail.com>
2018-02-04 12:14:37 -0500

    first draft compiles successfully

--- game.hs
+++ game.hs
@@ -3,23 +3,27 @@ module Game where
 import Player(Player(Player),PlayerType(Human),prompt,hasLost)
 import Piece(Color(White,Black))
 import Move(Move(Move))
+import Display(initializeDisplay,clearDisplay)
 
-type Game = [Move]
+data Game = Game { log :: Log, player1 :: Player, player2 :: Player}
+type Log = [Move]
 
-currentPlayer :: Game -> Color
-currentPlayer game = if even (length game) then White else Black
+currentPlayer :: Log -> Color
+currentPlayer log = if even (length log) then White else Black
     where even = (\n -> n == 0 || mod n 2 == 0)
 
-play :: Game
-play = play' []
+getMove :: Player -> Log -> IO Move
+getMove player log = prompt player log
 
-play' :: Game -> Game
-play' game = if hasLost (currentPlayer game) game then game else play' (nextTurn game)
+play' :: Game -> IO ()
+play' (Game log pl1 pl2) = do if hasLost color log
+                              then return ()
+                              else do mv <- getMove cp log
+                                      play' (Game (mv : log) pl1 pl2)
+    where color = (currentPlayer log)
+          cp = if color == White then pl1 else pl2
 
-nextTurn :: Game -> Game
-nextTurn game = getMove game : game
-
-getMove :: Game -> Move
-getMove game = prompt player game
-    where color = currentPlayer game
-          player = (Player Human color)
+play :: IO ()
+play = do screen <- initializeDisplay
+          play' (Game [] (Player Human White) (Player Human Black))
+          clearDisplay screen

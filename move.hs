efd636db98c5e77e8deaf87ab5b928f8c0723bd0
Gabriel McDonald <gabrielmcdonald1@gmail.com>
2018-02-05 13:34:55 -0500

    en passant and fix pawn bugs

--- move.hs
+++ move.hs
@@ -31,10 +31,15 @@ getPos pos board = filter ((==pos) . position) board
 executeMove :: Move -> Board -> Board
 executeMove (Move s p) b = move s p b
 
+pawnTwoStep :: Move -> Bool
+pawnTwoStep (Move (Piece _ t (a,_)) (b,_)) = t == Pawn && (abs (a - b)) == 2
+
 move :: Piece -> Pos -> Board -> Board
 move (Piece White Pawn pos) (0,a) board = (Piece White Queen (0,a)) : (filter (/=(Piece White Pawn pos)) board)
 move (Piece Black Pawn pos) (7,a) board = (Piece Black Queen (7,a)) : (filter (/=(Piece Black Pawn pos)) board)
-move piece toPos board = (Piece (color piece) (pieceType piece) toPos) : (filter (\s -> s /= piece && (position s) /= toPos) board)
+move (Piece c t (x1,y1)) (x2,y2) board = if t == Pawn && (null (getPos (x2,y2) board)) && x1 /= y1 && x2 /= y2
+                                         then (Piece c t (x2,y2)) : (filter (\s -> (position s) /= (x1,y1) && (position s) /= (x1,y2)) board)
+                                         else (Piece c t (x2,y2)) : (filter (\s -> (position s) /= (x1,y1) && (position s) /= (x2,y2)) board)
 
 isOccupiedBy :: Pos -> Color -> Board -> Bool
 isOccupiedBy pos col board = (not (null occupier)) && (color (head occupier)) == col
@@ -66,20 +71,20 @@ pawnMoves pawn board = [(Move pawn pos) | pos <- forwardMoves pawn board ++ capt
 
 captureMoves :: Piece -> Board -> [Pos]
 captureMoves (Piece Black _ pos) board = filter (\p -> isOccupiedBy p White board) cornerMoves
-    where cornerMoves = filter onBoard [(diff pos (-1, -1)), (diff pos (-1,1))]
+    where cornerMoves = filter onBoard [(diff pos (1, -1)), (diff pos (1,1))]
 
 captureMoves (Piece White _ pos) board = filter (\p -> isOccupiedBy p Black board) cornerMoves
-    where cornerMoves = filter onBoard [(diff pos (1, -1)), (diff pos (1,1))]
+    where cornerMoves = filter onBoard [(diff pos (-1, 1)), (diff pos (-1,-1))]
 
 forwardMoves :: Piece -> Board -> [Pos]
 forwardMoves (Piece Black _ pos) board | (offBoard oneForward) || not (null (getPos oneForward board)) = []
-                                       | (offBoard twoForward) || not (null (getPos twoForward board)) = [oneForward]
+                                       | (fst pos) /= 1 || not (null (getPos twoForward board)) = [oneForward]
                                        | otherwise = [oneForward, twoForward]
     where oneForward = diff pos (1,0)
           twoForward = diff pos (2,0)
 
 forwardMoves (Piece White _ pos) board | (offBoard oneForward) || not (null (getPos oneForward board)) = []
-                                       | (offBoard twoForward) || not (null (getPos twoForward board)) = [oneForward]
+                                       | (fst pos) /= 6 || not (null (getPos twoForward board)) = [oneForward]
                                        | otherwise = [oneForward, twoForward]
     where oneForward = diff pos (-1,0)
           twoForward = diff pos (-2,0)
--- player.hs
+++ player.hs
@@ -1,7 +1,7 @@
 module Player where
 
-import Piece(Color,Pos)
-import Move(Move,executeMove,toPos)
+import Piece(Piece(Piece),Color,Pos,position,color)
+import Move(Move(Move),executeMove,toPos,pawnTwoStep)
 import Board(Board,startBoard,validMoves,isMated)
 import Display(Display(Display),getMove)
 
@@ -20,7 +20,34 @@ lastMove log = (toPos (head log))
 hasLost :: Color -> Log -> Bool
 hasLost col log = isMated col (currentBoard log)
 
+availableMoves :: Color -> Log -> [Move]
+availableMoves col log = (validMoves col board) ++ (specialMoves col log)
+    where board = currentBoard log
+
+specialMoves :: Color -> Log -> [Move]
+specialMoves col log = (castles col log) ++ (enPassant col log)
+
+castles :: Color -> Log -> [Move]
+castles col log = []
+
+enPassant :: Color -> Log -> [Move]
+enPassant col log = if pawnTwoStep lastMove && (not (null adj))
+                    then [(Move (head adj) (intermediary lastMove))]
+                    else []
+    where board = currentBoard log
+          lastMove = head log
+          adj = findAdjacent (toPos lastMove) col board
+
+findAdjacent :: Pos -> Color -> Board -> [Piece]
+findAdjacent pos col board = filter (\p -> (oneBeside (position p) pos) && (Piece.color p) == col) board
+
+oneBeside :: Pos -> Pos -> Bool
+oneBeside (x1,y1) (x2,y2) = x1 == x2 && (abs (y1 - y2)) == 1
+
+intermediary :: Move -> Pos
+intermediary (Move (Piece _ _ (x1,y)) (x2,_)) = ((x1 + x2) `div` 2, y)
+
 prompt :: Player -> Log -> IO Move
 prompt (Player Human col) log = getMove (Display board (lastMove log) moves)
     where board = currentBoard log
-          moves = validMoves col board
+          moves = availableMoves col log
